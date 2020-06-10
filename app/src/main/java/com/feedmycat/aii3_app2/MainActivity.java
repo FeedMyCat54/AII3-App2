a8882a3d3c833d33afe51a4de9ee363fb708284b
feedmycat <konstantangelou@gmail.com>
2020-06-10 16:42:51 +0300

    Add main map activity

--- app/src/main/java/com/feedmycat/aii3_app2/MainActivity.java
+++ app/src/main/java/com/feedmycat/aii3_app2/MainActivity.java
@@ -1,13 +1,28 @@
 package com.feedmycat.aii3_app2;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+  private GoogleMap map;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // Set up the map
+    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        .findFragmentById(R.id.map);
+    mapFragment.getMapAsync(this);
+  }
+
+
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    map = googleMap;
   }
 }
