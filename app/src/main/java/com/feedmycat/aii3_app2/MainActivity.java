6bd50861a1c326df4310f3f00b506340c9735776
feedmycat <konstantangelou@gmail.com>
2020-06-10 16:11:33 +0300

    first commit

---
+++ app/src/main/java/com/feedmycat/aii3_app2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.feedmycat.aii3_app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
